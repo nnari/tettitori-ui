06f18d98492020d0cbf96c9e8690f5b3417964b0
Tatu Pesonen <pannariiz@gmail.com>
2021-01-28 22:46:03 +0200

    add dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+//load dotfiles
+require('dotenv').config()
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
