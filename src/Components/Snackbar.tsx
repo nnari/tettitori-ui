a2c45582ebbb59ada6fcd7728ac5ce7e40fc1fa5
Tatu Pesonen <pannariiz@gmail.com>
2021-02-10 20:55:01 +0200

    Add animation to snackbar and run prettier

--- src/Components/Snackbar.tsx
+++ src/Components/Snackbar.tsx
@@ -1,47 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Icon, Message, IconProps, Segment } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import {
+  Icon,
+  Message,
+  IconProps,
+  Segment,
+  Transition,
+} from "semantic-ui-react";
+import "../index.css";
 
 const css: any = {
-    minWidth: "300px",
-    borderLeft: "solid 15px",
-    borderColor: "#32CD32",
-    borderRadius: "0 !important",
-    fontSize: "1.1em",
-    position: "fixed",
-    bottom: "10px",
-    right: "10px",
-    zIndex: 100,
-}
+  minWidth: "300px",
+  borderLeft: "solid 15px",
+  borderColor: "#32CD32",
+  borderRadius: "0 !important",
+  fontSize: "1.1em",
+  position: "fixed",
+  bottom: "10px",
+  right: "10px",
+  zIndex: 100,
+};
 
 interface Props {
-    timeout: number,
+  timeout: number;
 }
 
-let snackbarNotify = (message: string) => { };
+let snackbarNotify = (message: string) => {};
 
 const Snackbar = ({ timeout = 6000 }: Props) => {
-    const [visible, setVisible] = useState<boolean>(false);
-    const [message, setMessage] = useState<string>('');
-    
-    const openWithMessage = (message: string) => {
-        setMessage(message);
-        setVisible(true);
-        setTimeout(() => setVisible(false), timeout);
-    }
+  const [visible, setVisible] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-    snackbarNotify = openWithMessage;
-    return visible ? (
-        <Segment inverted style={css}>
-            <b>
-                {message}
-            </b>
-            <Icon
-                name="times"
-                style={{ float: "right", cursor: "pointer" }}
-                onClick={() => setVisible(false)}
-            />
-        </Segment>
-    ) : null;
-}
+  const openWithMessage = (message: string) => {
+    setMessage(message);
+    setVisible(true);
+    setTimeout(() => setVisible(false), timeout);
+  };
+
+  snackbarNotify = openWithMessage;
+  return (
+    <Transition visible={visible} animation="fade left" duration={500}>
+      <Segment inverted className="snackbar">
+        <b>{message}</b>
+        <Icon
+          name="times"
+          style={{ float: "right", cursor: "pointer" }}
+          onClick={() => setVisible(false)}
+        />
+      </Segment>
+    </Transition>
+  );
+};
 
 export { Snackbar, snackbarNotify };
