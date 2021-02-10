82dcc844dbf51d416000888e6f6e03e94ac20b6b
Tatu Pesonen <pannariiz@gmail.com>
2021-02-10 20:51:37 +0200

    Add new API url to Endpoints.ts

--- src/Utility/Endpoints.ts
+++ src/Utility/Endpoints.ts
@@ -2,5 +2,6 @@ const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
 const LOGIN_URL = API_URL + "/login";
 const JOB_URL = API_URL + "/job";
 const DEGREE_URL = API_URL + "/degree";
+const ORIENTATION_URL = API_URL + "/orientation";
 
-export {API_URL, LOGIN_URL, JOB_URL, DEGREE_URL};
+export { API_URL, LOGIN_URL, JOB_URL, DEGREE_URL, ORIENTATION_URL };
