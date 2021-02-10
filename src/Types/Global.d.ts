ef3891d65f9e3b1f37107a7577b768736cc64dd7
Tatu Pesonen <pannariiz@gmail.com>
2021-02-10 20:52:04 +0200

    Add type for Orientation

--- src/Types/Global.d.ts
+++ src/Types/Global.d.ts
@@ -1,27 +1,35 @@
 declare interface User {
-    username: string,
-    role: "admin" | "workplace",
-    accessToken: string,
-    iat: Number
+  username: string;
+  role: "admin" | "workplace";
+  accessToken: string;
+  iat: Number;
 }
 
-declare module '*.jpg';
+declare module "*.jpg";
+declare module "*.css";
 
 declare interface Degree {
-    _id: string,
-    title: string,
-    __v: Number
+  _id: string;
+  title: string;
+  __v: Number;
+}
+
+declare interface ActivityOrientation {
+  _id: string;
+  title: string;
+  __v: Number;
 }
 
 declare interface Job {
-    _id: string,
-    title: string,
-    relevantDegrees: Degree[],
-    authorDisplayName: string,
-    author: string,
-    body: {
-        description: string,
-        contactInfo: string,
-        address: string,
-    },
+  _id: string;
+  title: string;
+  relevantDegrees: Degree[];
+  relevantOrientations: ActivityOrientation[];
+  authorDisplayName: string;
+  author: string;
+  body: {
+    description: string;
+    contactInfo: string;
+    address: string;
+  };
 }
