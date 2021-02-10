fb2446faff4fe55846308e6123babe775361948d
Tatu Pesonen <pannariiz@gmail.com>
2021-02-10 20:54:33 +0200

    Run prettier on Navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,32 +1,50 @@
-import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
-import { Menu, Container } from 'semantic-ui-react';
-import { LoginFormDropdown } from './LoginFormDropdown';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu, Container } from "semantic-ui-react";
+import { LoginFormDropdown } from "./LoginFormDropdown";
+import styles from "../index.css";
 
 interface Props {
-  position: 'bottom' | 'top',
-  handleAccessToken: (accessToken: string) => void,
-  handleLogOut: () => void,
-  user: User,
+  position: "bottom" | "top";
+  handleAccessToken: (accessToken: string) => void;
+  handleLogOut: () => void;
+  user: User;
 }
 
-export const NavBar = ({ position, handleAccessToken, user, handleLogOut }: Props) => {
+export const NavBar = ({
+  position,
+  handleAccessToken,
+  user,
+  handleLogOut,
+}: Props) => {
   return (
-    <Menu fixed={position} inverted size='large'>
+    <Menu fixed={position} inverted size="large">
       <Container>
         <Link to="/">
-          <Menu.Item as='a' header>
+          <Menu.Item as="a" header>
             Tettilä
-              </Menu.Item>
+          </Menu.Item>
         </Link>
-        <Link to="/paikat"><Menu.Item as='a'>Paikat</Menu.Item></Link>
+        <Link to="/paikat">
+          <Menu.Item as="a">Paikat</Menu.Item>
+        </Link>
         {user?.accessToken && (
-          <Link to="/profile"><Menu.Item as='a'><b>Muokkaa oman paikan tietoja</b></Menu.Item></Link>
+          <Link to="/profile">
+            <Menu.Item as="a">
+              <b>Muokkaa oman paikan tietoja</b>
+            </Menu.Item>
+          </Link>
         )}
-        {user?.accessToken ? <Menu.Item position='right' as='a' onClick={() => handleLogOut()}>Kirjaudu ulos</Menu.Item> : <Menu.Item position='right'><LoginFormDropdown handleAccessToken={handleAccessToken} /></Menu.Item>}
+        {user?.accessToken ? (
+          <Menu.Item position="right" as="a" onClick={() => handleLogOut()}>
+            Kirjaudu ulos
+          </Menu.Item>
+        ) : (
+          <Menu.Item position="right">
+            <LoginFormDropdown handleAccessToken={handleAccessToken} />
+          </Menu.Item>
+        )}
       </Container>
     </Menu>
-  )
-}
+  );
+};
