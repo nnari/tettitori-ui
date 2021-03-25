78b1c41644d20bb5eeeef2cbba5c3f4939a4ef7c
Tatu Pesonen <pannariiz@gmail.com>
2021-03-25 23:03:42 +0200

    Fix wildcard

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.USE_SERVER_PROXY === "true") {
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
